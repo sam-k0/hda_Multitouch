8c16a81ef2b5f69e5fce0699ab3b0145fbb6a29f
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2023-05-04 16:06:17 +0200

    ssu

--- P2/classes.py
+++ P2/classes.py
@@ -1,4 +1,5 @@
 import math
+import typing
 def distance(p1:tuple, p2:tuple):
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
@@ -23,38 +24,65 @@ class Touch:
 
 
 class Tracker:
-    def __init__(self) -> None:
-        self.currentFrameBlobs = list() # all blobs from this frame
-        self.touches = list()   # contains all touches registered rn
-        self.moveThreshold = 50 # in pixels
-        self.nextTouchID = 0    # next tracked touch gets this ID
+    def __init__(self, threshold:int=50) -> None:
+        self.currentFrameBlobs = list()  # all blobs from this frame
+        self.touches = list()          # contains all touches registered rn
+        self.moveThreshold = threshold       # in pixels
+        self.nextTouchID = 0                 # next tracked touch gets this ID
+        self.nextBlobID = 0
 
     #returns the blob that was used as the new blob for a touch
-    def doNearestNeighbor(self,currentTouch:Touch, newBlobs:list[Blob], threshold:int) -> Blob:
+    def doNearestNeighbor(self,currentTouch:Touch, allBlobs:list[Blob]) -> Blob:
         min_dist = float('inf')
         nearest = None
-        for blob in newBlobs:
+
+        for blob in allBlobs:
             dist = distance(blob.getTuple(), currentTouch.getTuple())
-            if dist > threshold: # if distance is too far, skip
-                # TODO: We probably also want to mark it as expired...
+            if dist > self.moveThreshold: # if distance is too far, skip
                 continue
             if dist < min_dist:
                 min_dist = dist
                 nearest = blob
-        
-        currentTouch.blob = nearest
-        return nearest
+            
+        return nearest # returns none when touch has no next blob
+
+    def addTouchToList(self, blob:Blob):
+        self.touches.append(Touch(self.nextTouchID, blob))
+        self.nextTouchID += 1
 
     #add to cache
-    def addBlobToFrame(self, blob:Blob)->None:
-        self.currentFrameBlobs.append(blob);
+    def addBlobToFrame(self, blobcoords:tuple)->None:
+        self.currentFrameBlobs.append(Blob(blobcoords, self.nextBlobID));
+        self.nextBlobID += 1
 
     #clear cache
     def clearCurrentFrame(self)->None:
         self.currentFrameBlobs.clear()
+        self.nextBlobID = 0
 
     # dont forget to update age on touches and destroy touches that are too old
     # or create new touches 
     # do nearest neighbor for all touches and the currentframeblobs
     def updateTouches(self):
-        pass
+        touch: Touch
+        usedBlobsForTouch = list() # list of all blobs that were used to update an existing touch
+
+        for touch in self.touches:
+            touch.ageFrames += 1
+            nearestBlob = self.doNearestNeighbor(touch, self.currentFrameBlobs)
+            if(nearestBlob is None):
+                print("Blob is none for touch: " + str(touch.id))
+                touch.ageFramesUntracked += 1
+            else:
+                touch.blob = nearestBlob # update blob
+
+
+        # loop over all current frame touches and see if there are 
+        # touches that didnt get used for an existing touch
+
+        cBlob: Blob
+        for cBlob in self.currentFrameBlobs:  # loop all blobs
+            if cBlob not in usedBlobsForTouch: #didnt get used
+                #create a new touch
+                self.addTouchToList(cBlob)
+                print("Adding blob to list of touches!")
--- P2/main.py
+++ P2/main.py
@@ -1,6 +1,7 @@
 import cv2
 from time import sleep
 import math
+import classes as p2
 background = None
 original = None
 MIN_THRESHOLD = 8
@@ -10,6 +11,7 @@ CONTOURS_VAL_MAX = 100
 COLOR_RED = (0,0,255)
 COLOR_GREEN = (0,255,0)
 
+myTracker = p2.Tracker()
 
 #cap = cv2.VideoCapture("C:\\Users\\Samuel\\Documents\\Studium\\MI - Multitouch Interfaces\\hda_Multitouch\\P1\\mt_camera_raw.AVI")
 cap = cv2.VideoCapture("E:\\14_STUDIUM\\Multitouch\\Praktikum\\mat\\mt_camera_raw.AVI")
@@ -24,6 +26,9 @@ if(cap):
             cv2.destroyAllWindows()
             print("Done.")
             break   
+
+        # Tracker settings
+        myTracker.clearCurrentFrame(); #clears all blobs
 
         original = img
         firstDiff = cv2.absdiff(background, img)
@@ -40,11 +45,17 @@ if(cap):
                     cEllipse = cv2.fitEllipse(contours[idx])
                     #### cEllipse[0] contains the center
                     # pass new to tracker list for this frame
-
+                    myTracker.addBlobToFrame(cEllipse[0])
                     
                     cv2.ellipse(original, cEllipse, COLOR_RED, 1, cv2.LINE_AA)
                     cv2.drawContours(original, contours, idx, COLOR_GREEN, 1, cv2.LINE_AA, hierarchy=hierarchy)
                     
+        # evaluate tracker
+        myTracker.updateTouches()
+
+        t: p2.Touch
+        for t in myTracker.touches:
+            cv2.putText(original, t.id, t.getTuple(), cv2.FONT_HERSHEY_COMPLEX, 1,COLOR_GREEN)
 
         
         cv2.imshow("Img", processed);
